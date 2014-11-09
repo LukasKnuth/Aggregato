0a811a6ac14520f4387d3f609ecc3bcc010c6c87
Lukas Knuth <mr.luke.187@googlemail.com>
2014-11-09 18:18:35 +0100

    The TMDB fetcher now fetches all episodes from all seasons of a show.

--- web_service/src/main/java/org/codeisland/aggregato/service/fetcher/impl/TMDBFetcher.java
+++ web_service/src/main/java/org/codeisland/aggregato/service/fetcher/impl/TMDBFetcher.java
@@ -8,6 +8,7 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.text.ParseException;
@@ -28,27 +29,70 @@ public class TMDBFetcher implements SeriesFetcher {
     private static final String API_KEY = "f3737c9013174480c625c67f4d84d741";
     private static final String API_VERSION = "3";
 
+    /**
+     * <p>Load a json object from the given {@code URL}.</p>
+     * <p>The returned object can be checked via {@code instanceof} to see id it's an object or an array.</p>
+     * @throws IOException if there was a problem establishing the connection or reading from
+     *  it (wrong URL?)
+     */
+    private static Object jsonFromUrl(URL url) throws IOException {
+        InputStream in = null;
+        try {
+            in = url.openStream();
+            return new JSONTokener(in).nextValue();
+        } finally {
+            if (in != null){
+                in.close();
+            }
+        }
+    }
+
     @Override
     public Series getSeries(String name){
+        List<Integer> ids = findSeries(name);
+        String series_id = String.valueOf(ids.get(0)); // TODO How do we decide here?
+
         try {
             URL url = new URL(String.format(
-                    BASE_URL+API_VERSION+"/search/tv?query=%s&api_key="+API_KEY, URLEncoder.encode(name, "UTF-8")
+                    BASE_URL+API_VERSION+"/tv/%s?api_key="+API_KEY, series_id
             ));
-            Object json = new JSONTokener(url.openStream()).nextValue();
+            Object json = jsonFromUrl(url);
             if (json instanceof JSONArray){
                 throw new RuntimeException("Expected a JSON Object, got an array...");
             }
 
-            JSONObject results = (JSONObject) json;
-            if (results.getInt("total_results") > 0){
-                JSONArray series = results.getJSONArray("results");
-                // TODO How do we decide here? Also, store the ID somewhere??
-                JSONObject first = series.getJSONObject(0);
-                return new Series(first.getString("name"), 0, String.valueOf(first.getInt("id")) );
-            } else {
-                return null;
+            JSONObject series = (JSONObject) json;
+
+            return new Series(
+                    series.getString("name"), series.getInt("number_of_seasons"), series_id
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns a list of TMDB id's for the given name, in the order they we're returned by the API.
+     */
+    private List<Integer> findSeries(String name) {
+        try {
+            URL url = new URL(String.format(
+                    BASE_URL+API_VERSION+"/search/tv?query=%s&api_key="+API_KEY, URLEncoder.encode(name, "UTF-8")
+            ));
+
+            Object json = jsonFromUrl(url);
+            if (json instanceof JSONArray){
+                throw new RuntimeException("Expected a JSON Object, got an array...");
             }
 
+            JSONObject result_object = (JSONObject) json;
+            JSONArray results = result_object.getJSONArray("results");
+            List<Integer> ids = new ArrayList<>(results.length());
+
+            for (int i = 0; i < results.length(); i++){
+                ids.add( results.getJSONObject(i).getInt("id") );
+            }
+            return ids;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -58,31 +102,33 @@ public class TMDBFetcher implements SeriesFetcher {
     public List<Episode> getEpisodes(Series series) {
         List<Episode> all_episodes = new ArrayList<>();
         try {
-            URL url = new URL(String.format(
-                    BASE_URL+API_VERSION+"/tv/%s/season/%s?api_key="+API_KEY, series.getTmdbId(), "1"
-            ));
-            Object json = new JSONTokener(url.openStream()).nextValue();
-            if (json instanceof JSONArray){
-                throw new RuntimeException("Expected a JSON Object, got an array...");
-            }
-
-            JSONObject seasons = (JSONObject) json;
-            JSONArray episodes = seasons.getJSONArray("episodes");
-            for (int i = 0; i < episodes.length(); i++){
-                JSONObject episode = episodes.getJSONObject(i);
-
-                Date air_date = null;
-                try {
-                    air_date = DATE_FORMAT.parse(episode.getString("air_date"));
-                } catch (ParseException e) {
-                    e.printStackTrace();
+            for (int season_nr = 1; season_nr <= series.getSeasons(); season_nr++){
+                URL url = new URL(String.format(
+                        BASE_URL+API_VERSION+"/tv/%s/season/%s?api_key="+API_KEY, series.getTmdbId(), season_nr
+                ));
+                Object json = jsonFromUrl(url);
+                if (json instanceof JSONArray){
+                    throw new RuntimeException("Expected a JSON Object, got an array...");
                 }
 
-                all_episodes.add(new Episode(
-                        series, episode.getString("name"),
-                        episode.getInt("episode_number"), episode.getInt("season_number"),
-                        air_date, episode.getString("overview")
-                ));
+                JSONObject season = (JSONObject) json;
+                JSONArray episodes = season.getJSONArray("episodes");
+                for (int i = 0; i < episodes.length(); i++){
+                    JSONObject episode = episodes.getJSONObject(i);
+
+                    Date air_date = null;
+                    try {
+                        air_date = DATE_FORMAT.parse(episode.getString("air_date"));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    all_episodes.add(new Episode(
+                            series, episode.getString("name"),
+                            episode.getInt("episode_number"), episode.getInt("season_number"),
+                            air_date, episode.getString("overview")
+                    ));
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
